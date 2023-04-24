065d48da1c9705a5f01436ead948ee314b0d34c9
Tyshkan4ik <001vat@gmail.com>
2023-04-25 00:55:52 +0300

    added a table

--- Birthdays/ViewController.swift
+++ Birthdays/ViewController.swift
@@ -8,11 +8,64 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: - Properties
+    
+    lazy var table: UITableView = {
+       let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        return table
+    }()
+    
+    //MARK: - Methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemYellow
+        setupElements()
+        setupConstraints()
     }
     
+    private func setupElements() {
+        view.addSubview(table)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            table.topAnchor.constraint(equalTo: view.topAnchor),
+            table.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            table.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            table.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    private func setupTable() {
+        table.delegate = self
+        table.dataSource = self
+    }
 }
 
+//MARK: - extension - UITableViewDataSource
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell
+        if let reuseCell = tableView.dequeueReusableCell(withIdentifier: "cell") {
+            cell = reuseCell
+        } else {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        }
+
+        return cell
+    }
+}
+
+//MARK: - extension - UITableViewDelegate
+
+extension ViewController: UITableViewDelegate {
+    
+}
