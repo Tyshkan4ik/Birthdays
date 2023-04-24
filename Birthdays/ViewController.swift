9f0740986e9c59cf6d456e3104beb2f2e0f37cf4
Tyshkan4ik <001vat@gmail.com>
2023-04-24 20:13:49 +0300

    start project

---
+++ Birthdays/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Birthdays
+//
+//  Created by Виталий Троицкий on 24.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemYellow
+    }
+    
+}
+
